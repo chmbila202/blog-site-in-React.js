3654528467f3d004e56ec82888402bd9c84115bf
Muhammad Bilal Sadiq <35162681+chmbila202@users.noreply.github.com>
2022-05-27 03:55:28 -0400

    Add files via upload

---
+++ blog in React.js/src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer>
+      <div className="footer">
+        <ul>
+          <li>React JS</li>
+          <li>React Router</li>
+          <li>Draft JS</li>
+          <li>Assignment:4</li>
+          <li>By <a href="https://github.com/chmbila202" rel="noopener noreferrer" target="_blank">Muhammad Bilal Sadiq</a></li>
+        </ul>
+      </div>
+    </footer>
+  )
+}
